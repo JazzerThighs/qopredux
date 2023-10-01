f2772649c8997ecf50f6690dec1f7cdf19407977
JazzerThighs <profenjb199@gmail.com>
2023-09-30 21:48:54 -0400

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/qrud/qr_input/mod.rs

--- src/qrud/qr_input/mod.rs
+++ src/qrud/qr_input/mod.rs
@@ -1,26 +1,20 @@
 pub struct ActionInput {
     pub kb_down: Vec<String>,
     pub kb_up: Vec<String>,
-    pub m_n_on: Vec<u8>,
-    pub m_n_off: Vec<u8>,
+    pub midi_msg: Vec<midi::Message>,
 }
 pub struct TranspositionKB {
     pub code: String,
-    pub note_bool: bool,
-    pub cent_bool: bool,
     pub note_delta: i16,
     pub cent_delta: f32,
 }
 pub struct TranspostitionMIDI {
-    pub msg: u8,
-    pub note_bool: bool,
-    pub cent_bool: bool,
+    pub msg: Vec<midi::Message>,
     pub note_delta: i16,
     pub cents_delta: f32,
 }
 pub struct TranspositionInput {
     pub kb_down: Vec<TranspositionKB>,
     pub kb_up: Vec<TranspositionKB>,
-    pub m_n_on: Vec<TranspostitionMIDI>,
-    pub m_n_off: Vec<TranspostitionMIDI>,
+    pub midi_msg: Vec<TranspostitionMIDI>,
 }
